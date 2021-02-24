8fc814dcad5a2fcf7c2be0218cb6c040af876d98
Josejulio Martínez <finwemartinez@gmail.com>
2021-02-24 07:37:57 -0600

    Update hvoice in new contract. (#13)
    * Update hvoice in new contract.
    
    * Add env for hypha-hvoice-token
    
    * Use the issueToken method instead of calling manually
    
    * Some problems accessing the logs, opening other file (nodeos-go.log)
     and piping the stdout/err to that file from go
    
    * Add voice contract and a test for migration.
    
    * More itests for hvoice operations (issue, transfer and decay)
    
    * Fix bugs, complete test

--- include/common.hpp
+++ include/common.hpp
@@ -131,6 +131,8 @@ namespace common
 #define HUSD_TOKEN_CONTRACT "husd_token_contract"
 #define TREASURY_CONTRACT "treasury_contract"
 #define HYPHA_TOKEN_CONTRACT "hypha_token_contract"
+#define HVOICE_TOKEN_CONTRACT "hvoice_token_contract"
+
 #define UPDATED_DATE "updated_date"
 #define SEEDS_DEFERRAL_FACTOR_X100 "seeds_deferral_factor_x100"
 #define HYPHA_DEFERRAL_FACTOR_X100 "hypha_deferral_factor_x100"
