e019c2a4adc2e7b1b904d94f53e0a41e74fbced8
Max <max@digitalscarcity.io>
2021-03-30 14:03:37 -0400

    added stats table

--- include/util.hpp
+++ include/util.hpp
@@ -41,6 +41,18 @@ namespace hypha
     typedef eosio::singleton<eosio::name("config"), configtable> configtables;
     typedef eosio::multi_index<eosio::name("config"), configtable> dump_for_config;
 
+    struct currency_stats {
+        eosio::asset    supply;
+        eosio::asset    max_supply;
+        eosio::name     issuer;
+        uint64_t decay_per_period_x10M;
+        uint64_t decay_period;
+
+        uint64_t primary_key()const { return supply.symbol.code().raw(); }
+    };
+    typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+
+
     // price history table is used to read the seeds price
     struct price_history_table
     {
