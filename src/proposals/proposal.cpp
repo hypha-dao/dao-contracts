efb41807ee2764875fd7bd582bd279a3d1cc1f5c
Josejulio Martínez <finwemartinez@gmail.com>
2021-04-09 17:14:28 -0500

    Prevent closing a proposal before expiration

--- src/proposals/proposal.cpp
+++ src/proposals/proposal.cpp
@@ -68,6 +68,13 @@ namespace hypha
 
     void Proposal::close(Document &proposal)
     {
+        auto expiration = proposal.getContentWrapper().getOrFail(BALLOT, EXPIRATION_LABEL, "Proposal has no expiration")->getAs<eosio::time_point>();
+
+        eosio::check(
+            eosio::time_point_sec(eosio::current_time_point()) > expiration,
+            "Voting is still active for this proposal"
+        );
+
         eosio::checksum256 root = getRoot(m_dao.get_self());
 
         Edge edge = Edge::get(m_dao.get_self(), root, proposal.getHash(), common::PROPOSAL);
