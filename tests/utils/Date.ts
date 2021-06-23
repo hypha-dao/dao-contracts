103965f97efc0723d6a4c00f489b236b658230c6
Josejulio Martínez <finwemartinez@gmail.com>
2021-06-23 17:44:54 -0500

    Write debug info

--- tests/utils/Date.ts
+++ tests/utils/Date.ts
@@ -1,4 +1,5 @@
 export const toISOString = (date: Date) => {
     // The testing framework always set the mills to 000 and doesn't have the Z
+    console.log('DEBUG: Writing the raw date', date.toISOString());
     return date.toISOString().split('.')[0] + '.000';
 };
