98d5a3f8858c6039e8232226b92beba6f68f5787
Josejulio Martínez <finwemartinez@gmail.com>
2021-08-19 13:27:03 -0500

    Truncate date instead of rounding (rounding isn't working on the server)

--- tests/utils/Date.ts
+++ tests/utils/Date.ts
@@ -13,10 +13,9 @@ export const fromUTC = (date: Date) => {
 }
 
 export const toISOString = (date: Date) => {
-    // The testing framework always rounds the  millis and doesn't have the Z
-    // 22:47:12.867Z becomes 22:47:13
+    // The testing framework truncates (?) the  millis and doesn't have the Z
+    // 22:47:12.867Z becomes 22:47:12
     const copy = new Date(date);
-    copy.setSeconds(Math.round(parseFloat(`${copy.getSeconds()}.${copy.getMilliseconds()}`)));
     copy.setMilliseconds(0);
     return copy.toISOString().split('.')[0] + '.000';
 };
@@ -24,6 +23,7 @@ export const toISOString = (date: Date) => {
 export const nextDay = (environment: DaoBlockchain, date: Date, offsetMinutes: number = 10) => {
   // Sets the time to the end of proposal
   const tomorrow = new Date();
+  tomorrow.setMilliseconds(0);
   tomorrow.setDate(date.getDate() + 1);
   tomorrow.setMinutes(tomorrow.getMinutes() + offsetMinutes);
   environment.setCurrentTime(tomorrow);
@@ -32,7 +32,8 @@ export const nextDay = (environment: DaoBlockchain, date: Date, offsetMinutes: n
 
 export const setDate = (environment: DaoBlockchain, date: Date, offsetMinutes: number = 10) => {
   const next = new Date(date);
-  //next.setMinutes(next.getMinutes() + offsetMinutes);
+  next.setMilliseconds(0);
+  next.setMinutes(next.getMinutes() + offsetMinutes);
   environment.setCurrentTime(next);
   environment.currentDate = next;
   return next;
