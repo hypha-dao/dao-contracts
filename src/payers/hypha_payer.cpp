85555f4dad100686a581c0fa6303a8043a1acf21
NIK <nheger@gmail.com>
2022-01-12 14:52:55 +0800

    deferred payment code

--- src/payers/hypha_payer.cpp
+++ src/payers/hypha_payer.cpp
@@ -8,6 +8,8 @@
 
 #include <common.hpp>
 
+// 3 year expiry
+#define EXPIRY_MILLISECONDS eosio::microseconds(3 * 365 * 24 * 60 * 60 * 1000)
 namespace hypha
 {
 
@@ -18,9 +20,19 @@ namespace hypha
         TRACE_FUNCTION()
         issueToken(m_dao.getSettingOrFail<eosio::name>(HYPHA_TOKEN_CONTRACT),
                    m_dao.get_self(),
-                   recipient,
+                   m_dao.getSettingOrFail<eosio::name>(HYPHA_COSALE_CONTRACT),
                    quantity,
-                   memo);
+                   "");
+
+        eosio::action(
+            eosio::permission_level{m_dao.get_self(), name("active")},
+            m_dao.getSettingOrFail<eosio::name>(HYPHA_COSALE_CONTRACT), name("createlock"),
+            std::make_tuple(m_dao.get_self(),
+                            recipient,
+                            quantity,
+                            memo,
+                            eosio::time_point(eosio::current_time_point().time_since_epoch() + EXPIRY_MILLISECONDS)))
+            .send();
 
         return Document(m_dao.get_self(),
                         m_dao.get_self(),
