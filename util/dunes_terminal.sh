c156c836718d0cfe537a7181d151b31ecf33cb1a
NIK <nheger@gmail.com>
2023-08-04 16:45:12 +0800

    cleanup

--- util/dunes_terminal.sh
+++ util/dunes_terminal.sh
@@ -12,7 +12,4 @@ HOST_PARENT_DIR="/host$PARENT_DIR"
 echo "The absolute path of the script's directory is: $SCRIPT_DIR"
 echo "The parent directory path is: $PARENT_DIR"
 
-# docker exec -it dune_container bash -c "cd '$HOST_PARENT_DIR'; bash"
-# docker exec -it dune_container bash -c "PS1='$(printf "\033[0;33m\w\033[0m\n> ")'; cd '$HOST_PARENT_DIR'; bash"
-# docker exec -it dune_container bash -c "PS1='\[\033[0;33m\]\w\n> \[\033[0m\]'; cd '$HOST_PARENT_DIR'; bash"
 docker exec -it dune_container bash -c "cd '$HOST_PARENT_DIR'; PS1='\[\033[0;33m\]\w\n> \[\033[0m\]' bash"
