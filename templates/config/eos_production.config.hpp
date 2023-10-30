7caf0014c52121721b9ef66f63bb8889c0ed3c6f
NIK <nheger@gmail.com>
2023-10-31 00:05:15 +0800

    add upvote elections

--- templates/config/eos_production.config.hpp
+++ templates/config/eos_production.config.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#define PRODUCTION_BUILD
+#define PRODUCTION_BUILD // seems this is unused
 #define EOS_BUILD
 #define USE_TREASURY
 #define USE_UPVOTE_ELECTIONS
-#define USE_PRICING_PLAN
+// #define USE_PRICING_PLAN
